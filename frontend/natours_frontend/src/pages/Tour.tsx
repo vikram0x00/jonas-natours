968fa8b849b29d62f8fb06477257dca569836bd7
Vikram <vikram0x00@proton.me>
2026-08-22 00:21:55 +0530

    Add Maptiler, Buggy

--- frontend/natours_frontend/src/pages/Tour.tsx
+++ frontend/natours_frontend/src/pages/Tour.tsx
@@ -5,6 +5,8 @@ import NotFound from "@/components/4041";
 import Header from "@/components/header1";
 import { useParams, useNavigate } from "react-router";
 import { MapPin, Calendar, Flag, User, Star, Tag } from "@mynaui/icons-react";
+import { Map, MapStyle, Marker } from "@maptiler/sdk";
+import "@maptiler/sdk/dist/maptiler-sdk.css";
 
 type Tour = {
 	createdAt: string,
@@ -46,14 +48,24 @@ const Tour = ()=>{
 		setTour(tours.find((e: any) => e.slug === slug));
 	}, []);
 
-	console.log(tour);
+	useEffect(()=>{
+		const map = new Map({
+			container: document.getElementById("map")!,
+			apiKey: import.meta.env.VITE_MAPTILER_APIKEY,
+			style: MapStyle.STREETS
+		});
+		const marker = new Marker();
+		tour?.locations.forEach(loc => {
+			marker.setLngLat(loc).addTo(map);
+		});
+	}, []);
 
 	return (
 		<>
 			<Header/>
 			{!tour && <NotFound type="Tour"/>}
 			{tour && <>
-			<section style={{ background: `url("http://localhost:3000/img/${tour.imageCover}")` }} className="h-screen w-full flex items-center justify-center flex-col bg-center bg-cover">
+			<section style={{ background: `url("http://localhost:3000/img/${tour.imageCover}")` }} className="p-60 mx-6 mt-3 rounded-2xl flex items-center justify-center flex-col bg-center bg-cover">
 				<h1 className="bg-black p-1 text-4xl font-semibold tracking-tighter">{tour.name}</h1>
 			</section>
 			<section className="flex items-center justify-center p-4">
@@ -63,17 +75,29 @@ const Tour = ()=>{
 					</div>
 					<div className="w-full m-2 rounded-2xl p-3 border bg-background">
 						<h1 className="font-medium text-lg mb-1">Tour Info</h1>
-						<span><MapPin className="mr-1" size={16}/>{tour.startLocation.description}</span>
-						<span><Calendar className="mr-1" size={16}/>{tour.startDates[0]}</span>
-						<span><Flag className="mr-1" size={16}/>{tour.locations}</span>
-						<span><User className="mr-1" size={16}/>{tour.maxGroupSize}</span>
-						<span><Star className="mr-1" size={16}/></span>
-						<span><Tag className="mr-1" size={16}/></span>
+						<p className="text-sm flex items-center my-1.5"><MapPin className="mr-1" size={16}/>{tour.startLocation.description}</p>
+						<p className="text-sm flex items-center my-1.5"><Calendar className="mr-1" size={16}/>{new Date(tour.startDates[0]).toString().slice(4, 15)}</p>
+						<p className="text-sm flex items-center my-1.5"><Flag className="mr-1" size={16}/>{tour.locations.length} stops</p>
+						<p className="text-sm flex items-center my-1.5"><User className="mr-1" size={16}/>{tour.maxGroupSize} people</p>
+						<p className="text-sm flex items-center my-1.5"><Star className="mr-1" size={16}/>{tour.ratingsAverage} ({tour.ratingsQuantity} ratings)</p>
+						<p className="text-sm flex items-center my-1.5"><Tag className="mr-1" size={16}/>${tour.price} (per person)</p>
 					</div>
 					<div className="w-full m-2 rounded-2xl p-3 border bg-background">
 						<h1 className="font-medium text-lg mb-1">Tour Guides</h1>
-						<p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus neque similique magnam! Porro adipisci sed voluptates quidem facilis iusto, eos incidunt, fugit laborum doloribus blanditiis. Porro ipsum totam autem dolore!</p>
+						<p className="text-sm">{tour.description}</p>
 					</div>
+			</section>
+			<section className="flex items-center justify-center flex-col">
+				<h1 className="font-medium text-2xl mx-2">Tour Images</h1>
+				<div className="flex items-center justify-center">
+					<img className="h-72 m-2 rounded-2xl" src={"http://localhost:3000/img/" + tour.images[0]} alt="Tour Image" />
+					<img className="h-72 m-2 rounded-2xl" src={"http://localhost:3000/img/" + tour.images[1]} alt="Tour Image" />
+					<img className="h-72 m-2 rounded-2xl" src={"http://localhost:3000/img/" + tour.images[2]} alt="Tour Image" />
+				</div>
+			</section>
+			<section className="flex items-center justify-center bg-muted mx-6 flex-col">
+				<h1 className="font-medium text-2xl">Tour Map</h1>
+				<div id="map" className="flex rounded-2xl bg-muted"></div>
 			</section>
 			</>}
 		</>
